7fca57ec295567b6518f0450514693fa2df75341
ctl <ctl@tal.com>
2022-04-17 10:20:22 +0800

    import fcl and script

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import { query } from "@onflow/fcl"
+
 
 function App() {
+  const [bookcount, setBookCount] = useState(0)
+
+  useEffect(() => {
+    const fetchBookCount = async () => {
+      try {
+        let res = await query({
+          cadence: `
+          import HelloWorld from 0xf4f8e2aa8f2ee51f
+  
+          pub fun main(): Int {
+          
+            let countRef = getAccount(0xf4f8e2aa8f2ee51f).getCapability(HelloWorld.BookShelfCountPath).borrow<&HelloWorld.BookShelf{HelloWorld.Count}>()
+                  ?? panic("can't borrow count bsf")
+                  
+              return countRef.getBookCount()
+          
+          }
+          `,
+        });
+        console.log(res)
+        setBookCount(res)
+      } catch (err) {
+        console.log("fetch book cnt err: ", err)
+      }
+    }
+    fetchBookCount()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +48,8 @@ function App() {
         >
           Learn React
         </a>
+        <br></br>
+        Fetch {bookcount} Books
       </header>
     </div>
   );
---
+++ src/config.js
@@ -0,0 +1,6 @@
+import { config } from "@onflow/fcl"
+
+config({
+  "accessNode.api": "https://access-testnet.onflow.org"
+  //"accessNode.api": "127.0.0.1:3569"
+})
