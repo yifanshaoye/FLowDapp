ec50f5216aa94e3b0e7d7105908f279d8e656d1f
ctl <ctl@tal.com>
2022-04-19 17:20:14 +0800

    check_user_collection

--- src/hooks/use-collection.hook.js
+++ src/hooks/use-collection.hook.js
@@ -1,8 +1,28 @@
-import { useState } from 'react'
+import { query } from '@onflow/fcl'
+import { useState, useEffect } from 'react'
+import { CHECK_COLLECTION } from '../flow/check-collection.script'
 
-export default function useCollection() {
-  const [loading] = useState(true)
+export default function useCollection(user) {
+  const [loading, setLoading] = useState(true)
   const [collection, setCollection] = useState(false)
+
+  useEffect(() => {
+    if(!user?.addr) return
+    const checkCollection = async () => {
+      try {
+        let res = await query({
+          cadence: CHECK_COLLECTION,
+          args: (arg, t) => [arg(user?.addr, t.Address)],
+        });
+        setCollection(res);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
+    };
+    checkCollection();
+  }, [])
 
   const createCollection = async () => {
     setCollection(true)
